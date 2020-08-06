927cecc09f44feeed74a5443abeb60ee379c645b
Rohith1996 <45393148+Rohith1996@users.noreply.github.com>
2020-08-06 16:47:25 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,11 @@
-import pysnow
+There are quite a few unsafe system followed in the warehouse and it has to be taken into serious note to avoid furthur loss and damage . 
+
+* First thing that i noticed was that the path which FLT's and employee used to use is very narrow small and hence sometime they collide with each other and cause damage . 
+
+* Then employed who used to operate FLT's are pretty young and don't have much experience in operating it effectively hence only people who are trained enough should be allowed to operate .
+
+* Mobile phones usage should be avoided while working in crucial situations .
+
+* In some places goods are staked in aisles and blocking designated walkways . They should be properly organised and kept so that blockage of paths can be avoided .
+
+* In recent accident there were oil spillage in ground due to which FLT skidded and fell on the goods aside . There should be quick action taken to clean spillage in path to avoid accidents henceforth .
