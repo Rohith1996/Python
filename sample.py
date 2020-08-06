dfef5fd7346c770eeb4eac679bb94c56c2867f63
Rohith1996 <45393148+Rohith1996@users.noreply.github.com>
2020-08-06 18:09:24 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,7 +1,15 @@
-Workers and managers have also given their suggestions and voluntary feedback about conditions that can made to improve the safety in this firm .
+Contractors have to possess some of the ability so as to be competence to do jobs like these .They primarily should be aware of safety and health guidelines before taking up the work . Following are some of the inquiries to be made to verify capabilities of contractors .
 
-* Management should inspect warehouse frequently and should take complaints raised by employees seriously . There should be written records of incidents if any happened so that they can claim to firm if same incidents occurs many times and steps to be taken to resolve it .
+* They should be enrolled to Employees Liability Compulsory Insurance policy , if anything happens to them they should be able to benefit through insurance claims . 
 
-* There should be proper training given to the employees and they shouldn't allow someone who isn't skilled enough to operate FLT that can cause huge accidents to workers and goods .
+* Copy of health and safety policy to be updated for every individual .
 
-* separate pathways should be made to Employee and FLT to enter the warehouse so that it can reduce the collision and damage .
+* Contractors must have the required KNOWLEDGE , TRAINING , ABILITY and EXPERIENCE ( KATE ) to be able to withhold jobs like these .Qualification of employees is mandatory and they should possess some work experience and membership of a professional organisation or certificate . Names of clients to which they have worked with previously to use it as a reference .
+
+* Accidents history and medical conditions of workers to be checked so that if any thing falls wrong we could be able to rectify as soon as possible without much further delay .
+
+* If any records of enforcement action taken against them to be analysed to be cautious about the state of contractors .
+
+* They should have proper plan of how they are going to handle this job , who will be responsible , how will the work be supervised , quality and check of equipment's to be used in work etc .
+
+* Finally they should be aware of the risks involved in doing this segregate of FLT's and Employees pathway so that they might be able to avoid the accidents from there end as much as possible 
